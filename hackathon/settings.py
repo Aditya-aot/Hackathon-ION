a14d974e0de60a00b4b2e610635f50b14e6a4542
Aditya-aot <aditya.aot.11@gmail.com>
2022-11-24 11:45:42 +0530

    vercel

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # for vercel
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static/images')
 # for vercel
 
 
